32b6a773022697f975f4b472af90e1b19d26d74b
X-MOD-I <deepanshumodi2000@gmail.com>
2022-11-14 23:57:28 +0530

    route changes

--- routes/User.js
+++ routes/User.js
@@ -2,10 +2,10 @@ const express = require("express");
 const userController = require("../controllers/User");
 const router = express.Router();
 
-router.get("/", userController.findAll); //show user list
-router.get("/:id", userController.findOne);
-router.post("/", userController.create);
-router.put("/:id", userController.update); //edit user functionality
-router.delete("/:id", userController.delete); //delete single user functionality
+router.get("/", userController.findAll); //get all users
+router.get("/:id", userController.findOne); //get user by id
+router.post("/", userController.create);  //create user
+router.put("/:id", userController.update); //edit user properties
+router.delete("/:id", userController.delete); //delete user
 
 module.exports = router;
